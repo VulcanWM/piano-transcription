5d70ce69c4ffbdf8e1f9d42cd5aedbd4a1dd896a
Medea <logmedha09@gmail.com>
2023-06-01 13:19:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import librosa
-import os
-import pathlib
 
 from piano_transcription_inference import PianoTranscription, sample_rate, load_audio
-from synthviz import create_video
 
-audio_input = 'my_audio.mp3'
-midi_intermediate_filename = 'transcription.mid'
-video_filename = 'output.mp4'
-
-transcriptor = PianoTranscription(device='cuda', checkpoint_path='./model.pth')
+audio_input = 'cut_listz.mp3'
+name = audio_input.split(".")[0]
+midi_intermediate_filename = f'{name}.mid'
+transcriptor = PianoTranscription(device='cpu', checkpoint_path='./model.pth')
 audio, _ = librosa.core.load(str(audio_input), sr=sample_rate)
-transcribed_dict = transcriptor.transcribe(audio, midi_intermediate_filename)
-create_video(input_midi=midi_intermediate_filename, video_filename=video_filename)
+transcriptor.transcribe(audio, midi_intermediate_filename)
